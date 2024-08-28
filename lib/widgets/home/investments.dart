472405c30a3e6c3e3315d3b7794d4916d010b19d
itiswd <itiswdev97@gmail.com>
2024-08-28 21:48:16 +0300

    Add Lock Icon and Investment Locking Feature
    - Introduced a new SVG lock icon asset and integrated it into the application.
    - Updated the `app_assets.dart` to include the new lock icon path for easy access across the app.
    - Created a new widget `InvestmentLockedRow` to display investment details with a lock icon, enhancing the UI for locked investments.
    - Modified the `investments.dart` widget to conditionally display additional investment details and lock indicators based on visibility settings.
    - Added the `intl` package to handle date formatting within the investment details display.
    - Updated `pubspec.yaml` to include the new `intl` dependency for date formatting support.

--- lib/widgets/home/investments.dart
+++ lib/widgets/home/investments.dart
@@ -1,8 +1,10 @@
 import 'package:capital_plus/core/constants/app_colors.dart';
 import 'package:capital_plus/core/constants/app_consts.dart';
 import 'package:capital_plus/core/utils/app_styles.dart';
+import 'package:capital_plus/widgets/home/investment_locked_row.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/intl.dart';
 import 'package:svg_flutter/svg.dart';
 
 class Investments extends StatelessWidget {
@@ -12,53 +14,87 @@ class Investments extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool visible = true;
+    final myDateTime = DateTime.now(); // Replace with your actual DateTime
+    final formattedDate = DateFormat('dd-MM-yyyy').format(myDateTime);
     return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: List.generate(
         5,
         (index) {
           return SizedBox(
-            height: 82.0.h,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SvgPicture.asset(
-                        width: 50.0.w,
-                        height: 50.0.h,
-                        investments['thumbnail'][index],
-                      ),
-                      SizedBox(width: 12.0.w),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            investments['name'][index],
-                            style: AppStyles.body1Medium,
+            height: visible == true ? 184.0.h : 82.0.h,
+            child: Stack(
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 8.0.h),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SvgPicture.asset(
+                          width: 50.0.w,
+                          height: 50.0.h,
+                          investments['thumbnail'][index],
+                        ),
+                        SizedBox(width: 12.0.w),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                investments['name'][index],
+                                style: AppStyles.body1Medium,
+                              ),
+                              SizedBox(height: 4.0.h),
+                              Text(
+                                investments['userName'][index],
+                                style: AppStyles.body2Regular,
+                              ),
+                              SizedBox(height: 4.0.h),
+                              Visibility(
+                                visible: visible,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      formattedDate,
+                                      style: AppStyles.body2Regular,
+                                    ),
+                                    Text(
+                                      'Investment user text',
+                                      style: AppStyles.body2Regular,
+                                    ),
+                                    const InvestmentLockedRow(),
+                                    SizedBox(height: 4.0.h),
+                                    const InvestmentLockedRow(),
+                                    SizedBox(height: 4.0.h),
+                                    const InvestmentLockedRow(),
+                                  ],
+                                ),
+                              ),
+                            ],
                           ),
-                          SizedBox(height: 4.0.h),
-                          Text(
-                            investments['userName'][index],
-                            style: AppStyles.body2Regular,
-                          ),
-                        ],
-                      ),
-                      const Spacer(),
-                      Text(
-                        '\$${investments['amount'][index]}',
-                        style: AppStyles.body1Medium,
-                      )
-                    ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8.0.h),
+                    const Divider(
+                      color: AppColor.grey,
+                      thickness: 0.5,
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 16.0.h,
+                  right: 16.0.w,
+                  child: Text(
+                    '\$${investments['amount'][index]}',
+                    style: AppStyles.body1Medium,
                   ),
-                  const Divider(
-                    color: AppColor.grey,
-                    thickness: 0.5,
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
           );
         },
