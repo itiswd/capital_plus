1a1323858a6d11b9369254a60086eedf1d37d907
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-28 21:50:22 +0300

    create AddInvestmentViewAppBar

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
-  
+
   /// Assets for iconsButtonOpen
   /// assets/icons/button_open.svg
   static const String iconsButtonOpen = "assets/icons/button_open.svg";
@@ -28,38 +28,47 @@ class Assets {
 
   /// Assets for iconsInvestmentBondsIconicon
   /// assets/icons/investment_bonds_iconicon.svg
-  static const String iconsInvestmentBondsIconicon = "assets/icons/investment_bonds_iconicon.svg";
+  static const String iconsInvestmentBondsIconicon =
+      "assets/icons/investment_bonds_iconicon.svg";
 
   /// Assets for iconsInvestmentCashIcon
   /// assets/icons/investment_cash_icon.svg
-  static const String iconsInvestmentCashIcon = "assets/icons/investment_cash_icon.svg";
+  static const String iconsInvestmentCashIcon =
+      "assets/icons/investment_cash_icon.svg";
 
   /// Assets for iconsInvestmentCryptoIcon
   /// assets/icons/investment_crypto_icon.svg
-  static const String iconsInvestmentCryptoIcon = "assets/icons/investment_crypto_icon.svg";
+  static const String iconsInvestmentCryptoIcon =
+      "assets/icons/investment_crypto_icon.svg";
 
   /// Assets for iconsInvestmentOtherIcon
   /// assets/icons/investment_other_icon.svg
-  static const String iconsInvestmentOtherIcon = "assets/icons/investment_other_icon.svg";
+  static const String iconsInvestmentOtherIcon =
+      "assets/icons/investment_other_icon.svg";
 
   /// Assets for iconsInvestmentStocksIcon
   /// assets/icons/investment_stocks_icon.svg
-  static const String iconsInvestmentStocksIcon = "assets/icons/investment_stocks_icon.svg";
+  static const String iconsInvestmentStocksIcon =
+      "assets/icons/investment_stocks_icon.svg";
 
   /// Assets for iconsLogoGroupSplash
   /// assets/icons/logo_group_splash.svg
-  static const String iconsLogoGroupSplash = "assets/icons/logo_group_splash.svg";
+  static const String iconsLogoGroupSplash =
+      "assets/icons/logo_group_splash.svg";
 
   /// Assets for iconsLogoOnboarding1
   /// assets/icons/logo_onboarding_1.svg
-  static const String iconsLogoOnboarding1 = "assets/icons/logo_onboarding_1.svg";
+  static const String iconsLogoOnboarding1 =
+      "assets/icons/logo_onboarding_1.svg";
 
   /// Assets for iconsLogoOnboarding2
   /// assets/icons/logo_onboarding_2.svg
-  static const String iconsLogoOnboarding2 = "assets/icons/logo_onboarding_2.svg";
+  static const String iconsLogoOnboarding2 =
+      "assets/icons/logo_onboarding_2.svg";
 
   /// Assets for iconsLogoOnboarding3
   /// assets/icons/logo_onboarding_3.svg
-  static const String iconsLogoOnboarding3 = "assets/icons/logo_onboarding_3.svg";
+  static const String iconsLogoOnboarding3 =
+      "assets/icons/logo_onboarding_3.svg";
+  static const String arrowBack = "assets/icons/arrow-back.svg";
 }
-
---
+++ lib/screens/add_investment_view.dart
@@ -0,0 +1,13 @@
+import 'package:capital_plus/widgets/home/add_investment_body.dart';
+import 'package:flutter/material.dart';
+
+class AddInvestmentView extends StatelessWidget {
+  const AddInvestmentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AddInvestmentBody(),
+    );
+  }
+}
