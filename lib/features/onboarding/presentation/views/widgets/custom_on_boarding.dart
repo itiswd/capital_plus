57f77650019413702d7dc4c5d539719d9ed697b6
itiswd <itiswdev97@gmail.com>
2024-08-29 14:10:56 +0300

    Refactor CustomOnBoarding widget for better readability and alignment
    - Adjusted the main axis alignment of the Column widget to start, ensuring consistent layout behavior.
    - Wrapped the PageView.builder parameters in a more readable format, improving code structure and maintainability.
    - Ensured consistent indentation and spacing within the itemBuilder method, enhancing overall code clarity.

--- lib/features/onboarding/presentation/views/widgets/custom_on_boarding.dart
+++ lib/features/onboarding/presentation/views/widgets/custom_on_boarding.dart
@@ -18,33 +18,35 @@ class CustomOnBoarding extends StatelessWidget {
   Widget build(BuildContext context) {
     return Expanded(
       child: PageView.builder(
-          controller: _pageController,
-          itemCount: customItemList.length,
-          physics: const NeverScrollableScrollPhysics(),
-          itemBuilder: (context, index) {
-            return Column(
-              children: [
-                customItemList[index].svgPicture,
-                SizedBox(height: 52.0.h),
-                Text(
-                  customItemList[index].title,
-                  style: AppStyles.header1.copyWith(
-                    fontSize: 22.sp,
-                    fontFamily: 'Outfit',
-                  ),
-                  textAlign: TextAlign.center,
+        controller: _pageController,
+        itemCount: customItemList.length,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              customItemList[index].svgPicture,
+              SizedBox(height: 52.0.h),
+              Text(
+                customItemList[index].title,
+                style: AppStyles.header1.copyWith(
+                  fontSize: 22.sp,
+                  fontFamily: 'Outfit',
                 ),
-                SizedBox(height: 24.h),
-                Text(
-                  customItemList[index].subtitle,
-                  style: AppStyles.body1Regular.copyWith(
-                    color: AppColor.darkGrey,
-                  ),
-                  textAlign: TextAlign.center,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 24.h),
+              Text(
+                customItemList[index].subtitle,
+                style: AppStyles.body1Regular.copyWith(
+                  color: AppColor.darkGrey,
                 ),
-              ],
-            );
-          }),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
