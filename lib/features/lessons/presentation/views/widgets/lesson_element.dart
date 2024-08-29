58c546d7b350a1967799454e599c5c98f24607c8
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-29 20:23:03 +0300

    cerate cardsstaticdata

--- lib/features/lessons/presentation/views/widgets/lesson_element.dart
+++ lib/features/lessons/presentation/views/widgets/lesson_element.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../../manager/lesson_element_state.dart';
 import '../../../../../core/utils/app_styles.dart';
