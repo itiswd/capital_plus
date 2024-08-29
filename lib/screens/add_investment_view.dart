ce84a6b966eeb77cd5ac0ae15009ae0edf34a390
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-29 11:40:09 +0300

    add comment

---
+++ lib/screens/add_investment_view.dart
@@ -0,0 +1,14 @@
+import 'package:capital_plus/widgets/add_investment/add_investment_body.dart';
+import 'package:flutter/material.dart';
+
+class AddInvestmentView extends StatelessWidget {
+  const AddInvestmentView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AddInvestmentBody(),
+    );
+  }
+}
+//klkfkn
