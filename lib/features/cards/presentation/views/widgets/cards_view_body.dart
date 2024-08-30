3eed08eb9ac8189782c3410a48e86dc6e0dd767b
Ahmed Yahia <ahmedyehia.122a@gmail.com>
2024-08-30 14:11:39 +0300

    refactor

--- lib/features/cards/presentation/views/widgets/cards_view_body.dart
+++ lib/features/cards/presentation/views/widgets/cards_view_body.dart
@@ -1,19 +1,23 @@
 import 'package:capital_plus/features/cards/presentation/views/widgets/cards_view_app_bar.dart';
 import 'package:capital_plus/features/cards/presentation/views/widgets/custom_card_items_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CardsViewBody extends StatelessWidget {
   const CardsViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: CustomScrollView(
         slivers: [
-          SliverToBoxAdapter(
+          const SliverToBoxAdapter(
             child: CardsViewAppBar(),
           ),
-          CustomCardItemsList(),
+          SliverToBoxAdapter(
+            child: SizedBox(height: 24.h),
+          ),
+          const CustomCardItemsList(),
         ],
       ),
     );
