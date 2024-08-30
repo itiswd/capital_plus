5520f59a5ec272c8a129019d337ae42f75d4ba55
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-30 19:57:48 +0300

    ref

--- lib/features/cards/data/static/company_data_static.dart
+++ lib/features/cards/data/static/company_data_static.dart
@@ -15,9 +15,45 @@ List<CompanyDataModel> companyData = [
         subtitle:"Trailing total returns as of 8/23/2024, which may include dividends or other distributions. Benchmark is S&P 500",
         yearReturn1: YearReturn(
             nameofCompany: "AAPL", rateofCompany: 26.58, rateofS$P500: 27.58),
-        yearReturn2: YearReturn(
-            nameofCompany: "AAPL", rateofCompany: 57.19, rateofS$P500: 25.01),
         yearReturn3: YearReturn(
+            nameofCompany: "AAPL", rateofCompany: 57.19, rateofS$P500: 25.01),
+        yearReturn5: YearReturn(
             nameofCompany: "AAPL", rateofCompany: 385.97, rateofS$P500: 97.42),
       )),
+        CompanyDataModel(
+      companyInformationModel: CompanyInformationModel(
+        subtitle:"Amazon.com, Inc. - is one of the world's largest companies specializing in e-commerce, cloud computing, artificial intelligence, and digital streaming. The company was founded by Jeff Bezos in 1994 and began as an online bookstore. Today, Amazon offers a wide range of products and services, including a platform for third-party sellers, streaming services Amazon Prime Video, Amazon Music, and Amazon Web Services (AWS), a leading cloud platform",
+        fullTimeEmployees: "1,525,000",
+        sector: "Consumer Cyclical",
+      ),
+      companyHistory:"Amazon began in 1994 as an online bookstore, operating out of Jeff Bezos' garage in Seattle. Bezos chose the name “Amazon” after the largest river in the world to emphasize the scale of the company. By 1997, Amazon became a publicly traded company, and by the late 1990s, the company had expanded into selling electronics, toys, and other products. In the 2000s, Amazon continued to grow by launching its AWS cloud platform, which was a key factor in its success",
+      interstingFacts:"Amazon Prime, launched in 2005, has been a key growth driver for the company, offering users free shipping and access to a wide range of digital services for a flat annual fee. As of 2023, more than 200 million people worldwide are subscribed to Amazon Prime. The service has significantly increased customer loyalty, boosted sales revenue and enabled Amazon to dominate the e-commerce industry.",
+      trailingModel: TrailingModel(
+        subtitle:"Trailing total returns as of 8/23/2024, which may include dividends or other distributions. Benchmark is S&P 500",
+        yearReturn1: YearReturn(
+            nameofCompany: "AMZN", rateofCompany: 31.79, rateofS$P500:27.51 ),
+        yearReturn3: YearReturn(
+            nameofCompany: "AMZN", rateofCompany: 6.47, rateofS$P500: 24.95),
+        yearReturn5: YearReturn(
+            nameofCompany: "AMZN", rateofCompany: 100.76, rateofS$P500: 97.32),
+      )),
 ];
+
+
+      //  CompanyDataModel(
+      // companyInformationModel: CompanyInformationModel(
+      //   subtitle:"",
+      //   fullTimeEmployees: "",
+      //   sector: "",
+      // ),
+      // companyHistory:"",
+      // interstingFacts:"",
+      // trailingModel: TrailingModel(
+      //   subtitle:"",
+      //   yearReturn1: YearReturn(
+      //       nameofCompany: "", rateofCompany: 26.58, rateofS$P500: 27.58),
+      //   yearReturn3: YearReturn(
+      //       nameofCompany: "", rateofCompany: 57.19, rateofS$P500: 25.01),
+      //   yearReturn5: YearReturn(
+      //       nameofCompany: "", rateofCompany: 385.97, rateofS$P500: 97.42),
+      // )),
